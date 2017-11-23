d55e756f4cbd559a0c88f13fc2ae8678eb3f97d5
Rie. K <riekoay582@ed.amdsb.ca>
2017-11-23 13:33:27 -0500

    Add project files.

---
+++ Draw Triangle/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Draw_Triangle
+{
+    partial class mainDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // mainDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 334);
+            this.Name = "mainDisplay";
+            this.Text = "Drawing Triangle ";
+            this.Load += new System.EventHandler(this.mainDisplay_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Draw Triangle/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw_Triangle
+{
+    public partial class mainDisplay : Form
+    {
+        Pen drawpen;
+        Graphics g;
+
+        public mainDisplay()
+        {
+            InitializeComponent();
+            
+        }
+        public void drawTriangle(double xOne, double xTwo, double xThree, double yOne, double yTwo, double yThree)
+        {
+            Graphics g = this.CreateGraphics();
+            g.DrawLine(drawpen, xOne, yOne, xTwo, yTwo);
+        }
+        private void mainDisplay_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
